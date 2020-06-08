68dec75fd75e68ab02dd70872fb3fa8260e8b91e
Connor Lake <vertoforce@gmail.com>
2020-06-08 13:18:34 -0400

    Added typecast

--- backends/airtablecrm/create.go
+++ backends/airtablecrm/create.go
@@ -10,8 +10,11 @@ import (
 func (c *Client) CreateItem(ctx context.Context, i *crm.Item) error {
 	request := struct {
 		Fields map[string]interface{} `json:"fields"`
+		// Typecast allows new options to be created with them multiple select field type
+		Typecast bool `json:"typecast"`
 	}{
-		Fields: i.Fields,
+		Fields:   i.Fields,
+		Typecast: true,
 	}
 	return c.client.CreateRecord(c.tableName, request)
 }
