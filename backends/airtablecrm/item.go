754782f9316ac8c5b55324c4b66a84d4f474e0fd
Connor Lake <vertoforce@gmail.com>
2020-06-16 18:50:03 -0400

    Added airtable crm attachment field

--- backends/airtablecrm/item.go
+++ backends/airtablecrm/item.go
@@ -6,6 +6,13 @@ type Item struct {
 	Fields     map[string]interface{}
 }
 
+// Attachment is a field type to upload an attachment
+//
+// So if you want to upload an attachment, set this as on of the fields when calling CreateRecord
+type Attachment struct {
+	URL string `json:"url"`
+}
+
 // GetFields of this item
 func (i *Item) GetFields() map[string]interface{} {
 	return i.Fields
